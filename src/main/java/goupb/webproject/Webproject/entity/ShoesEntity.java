90e4ee603ec9aa5edc459f70d37d0d0bec1dccae
K Patrick <kovacspatrik02@gmail.com>
2023-04-07 10:17:46 +0200

    added shoes entity class to the project

---
+++ src/main/java/goupb/webproject/Webproject/entity/ShoesEntity.java
@@ -0,0 +1,17 @@
+package goupb.webproject.Webproject.entity;
+
+import java.util.List;
+
+public class ShoesEntity {
+    private String brand;
+    private String name;
+    private String type;
+
+    private List<Integer> sizes;
+
+    private String gender;
+
+    private int price;
+    private String color;
+
+}
