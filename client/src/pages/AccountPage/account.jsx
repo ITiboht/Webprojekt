300070bca7eb21e433528389c4137b6becb06286
Quevadish <majoka1234561@gmail.com>
2023-05-24 14:54:53 +0200

    Account page beginnings

--- client/src/pages/AccountPage/account.jsx
+++ client/src/pages/AccountPage/account.jsx
@@ -3,8 +3,38 @@ import './account.css'
 
 export default function Account() {
   return (
-    <div className='Account-Page'>
-        <h1>Account</h1>
-    </div>
+    <div className="form">
+    <form>
+      <div className="input-container">
+        <label>Username </label>
+        <input type="text" name="uname" required />
+      </div>
+      <div className="input-container">
+        <label>Password </label>
+        <input type="password" name="pass" required />
+      </div>
+      <div className="button-container">
+        <input type="submit" />
+      </div>
+    </form>
+  </div>
   )
 }
+
+function renderForm() {
+  <div className="form">
+    <form>
+      <div className="input-container">
+        <label>Username </label>
+        <input type="text" name="uname" required />
+      </div>
+      <div className="input-container">
+        <label>Password </label>
+        <input type="password" name="pass" required />
+      </div>
+      <div className="button-container">
+        <input type="submit" />
+      </div>
+    </form>
+  </div>
+};
